fb5241e2cf7b60a29ec3bef2ef936437790677fe
ahmedibra24 <ai239128@gmail.com>
2026-04-22 17:11:07 +0200

    connect db

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,29 +2,45 @@
 set -e
 
 echo "Caching config..."
+php artisan config:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-echo "Waiting for MySQL..."
+echo "Waiting for TiDB..."
+
+MAX_TRIES=10
+COUNTER=0
 
 until php -r "
 try {
-    new PDO('mysql:host=' . getenv('DB_HOST') . ';port=' . getenv('DB_PORT') . ';dbname=' . getenv('DB_DATABASE'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'));
+    new PDO(
+        'mysql:host=' . getenv('DB_HOST') . ';port=' . getenv('DB_PORT') . ';dbname=' . getenv('DB_DATABASE') . ';sslmode=VERIFY_CA',
+        getenv('DB_USERNAME'),
+        getenv('DB_PASSWORD'),
+        [
+            PDO::MYSQL_ATTR_SSL_CA => getenv('MYSQL_ATTR_SSL_CA')
+        ]
+    );
+    echo 'DB Connected';
 } catch (Exception \$e) {
+    echo \$e->getMessage();
     exit(1);
 }
 "; do
-  echo "Database not ready... sleeping"
+  COUNTER=$((COUNTER+1))
+  echo "Database not ready... attempt $COUNTER/$MAX_TRIES"
   sleep 3
+
+  if [ $COUNTER -ge $MAX_TRIES ]; then
+    echo "Skipping DB wait..."
+    break
+  fi
 done
 
-echo "Database is ready ✔"
-
 echo "Running migrations..."
-php artisan migrate --force
-php artisan db:seed --force
-
+php artisan migrate --force || echo "Migration failed"
+php artisan db:seed --force || echo "Seeding failed"
 
 echo "Starting Laravel server..."
 exec php -S 0.0.0.0:80 -t public
